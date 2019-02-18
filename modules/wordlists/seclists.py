587a69fa465253c27a3916bf7c88f3d258537241
Calum Boal <calum.boal@onsecurity.co.uk>
2019-02-18 12:21:54 +0000

    Added SecLists

---
+++ modules/wordlists/seclists.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for b374k
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Calum Boal (@CalumBoal)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update SecLists, a collection of pentesting wordlists"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/danielmiessler/SecLists.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="SecLists"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# DEPENDS FOR ARCH INSTALLS
+ARCH="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# LAUNCHER
+LAUNCHER=""
