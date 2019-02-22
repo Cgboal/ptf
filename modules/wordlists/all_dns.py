0cf8e9e584cf867ddbbf018f6fa1c52cd96eafa8
Calum Boal <calum.boal@onsecurity.co.uk>
2019-02-22 19:41:25 +0000

    Added more wordlists

---
+++ modules/wordlists/all_dns.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Jason Haddix's all.txt DNS wordlist
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Calum Boal (@CalumBoal)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update all.txt a massive collection of DNS subdomains"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://gist.github.com/jhaddix/86a06c5dc309d08580a018c66354a056"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="AllDNS"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# DEPENDS FOR ARCH INSTALLS
+ARCH="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# LAUNCHER
+LAUNCHER=""
---
+++ modules/wordlists/probable-wordlists.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Probable Wordlists
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Calum Boal (@CalumBoal)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Probable Wordlsits, a large collection of wordlists and masks, requires additional downloads of large wordlists via bittorrent"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/berzerk0/Probable-Wordlists.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="ProbableWordlists"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# DEPENDS FOR ARCH INSTALLS
+ARCH="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# LAUNCHER
+LAUNCHER=""
